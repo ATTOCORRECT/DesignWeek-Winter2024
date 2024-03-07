4cfebca5cac065830231d54f1c752d984dde79a9
SrSiestas15 <148352083+SrSiestas15@users.noreply.github.com>
2024-03-07 17:22:43 -0500

    Edited everything so build will run

--- microMix/Assets/Team28/Scripts/ScanPlayerController.cs
+++ microMix/Assets/Team28/Scripts/ScanPlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.ShaderKeywordFilter;
+//using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 namespace team28
